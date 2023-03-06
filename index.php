4bc7e367f0a882649184259f209be3a991936c54
Demirichell <121028877+Demirichell@users.noreply.github.com>
2023-03-06 16:32:05 +0100

    nav
    .

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/main.css">
     <title>sushi</title>
 </head>
+
 <body>
+    <nav>
+        <h1>Home Menu About us</h1> 
+       
+       
+        <h2>Log-in </h2>
+    </nav>
     <section>
-   <nav>
-    <p></p>
-   </nav>
+
     </section>
-   
+
 </body>
+
 </html>
