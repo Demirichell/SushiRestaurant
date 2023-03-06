2258b27a9f605cac413121126c0776a914d931b9
Demirichell <121028877+Demirichell@users.noreply.github.com>
2023-03-06 12:03:41 +0100

    .

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <link rel="stylesheet" href="main.css">
+<link rel="stylesheet" href="./main.css">
     <title>sushi</title>
 </head>
 <body>
-    
+    <section>
+    <header>
+        <p>joe</p>
+    </header>
+    </section>
 </body>
 </html>
