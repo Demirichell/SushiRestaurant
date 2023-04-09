49df2c6cef80d34c8267dc46651d485230c1acfa
Demirichell <121028877+Demirichell@users.noreply.github.com>
2023-04-09 22:52:05 +0200

    adminupdate
    .

--- admin.php
+++ admin.php
@@ -1,42 +1,55 @@
-<?php include_once ('includes/connect.php');
+<?php include_once('includes/connect.php');
 //var_dump($_SESSION);
-if ( isset($_SESSION ['user_logged_in'])&& $_SESSION ['user_logged_in']== true) {
-   
-}
-else {
+if (isset($_SESSION['user_logged_in']) && $_SESSION['user_logged_in'] == true) {
+} else {
     header("location: inlog.php");
 }
 ?>
 
-<?php //include_once ('header.php');?>
-<a href="logout.php"> logout </a>
-<main>
+<?php //include_once ('header.php');
+?>
 
-<?php
-        include_once "./includes/connect.php";
+<!DOCTYPE html>
+<html lang="en">
 
-        $stmt = $conn->prepare("SELECT * FROM gerechten;");
-        $stmt->execute();
-        $data = $stmt->fetchAll();
-        //var_dump($data);
+<head>
+    <link rel="stylesheet" href="./css/admin.css">
+    <title>Admin</title>
+</head>
+
+<body>
+
+    <nav>
+
+    <button> <a href="logout.php"> logout </a></button>
+
+    </nav>
+    <?php
+    include_once "./includes/connect.php";
+
+
+    $stmt = $conn->prepare("SELECT * FROM gerechten;");
+    $stmt->execute();
+    $data = $stmt->fetchAll();
+    //var_dump($data);
+    ?>
+    <div class="bigsushi">
+        <?php
+        foreach ($data as $key => $value) :
         ?>
-        <div class="menu">
-            <?php
-            foreach ($data as $key => $value) :
-            ?>
-                <div class="smallmenu">
-                    <img class="sushi" src="<?php echo $value['img']; ?> " alt="gerechten">
-                    <p> <?php echo $value['naam']; ?> <br>
-                        <?php echo $value['prijs']; ?> <br>
-                        <?php echo $value['beschrijving']; ?> <br>
-                        <a href="delete.php?id=<?php echo $value['id']; ?>">delete</a> 
-                    
-                    </p>
+            <div class="functionmenu">
+                <img class="sushi" src="<?php echo $value['img']; ?> " alt="gerechten">
+                <p> <?php echo $value['naam']; ?> <br>
+                    <?php echo $value['prijs']; ?> <br>
+                    <?php echo $value['beschrijving']; ?> <br>
+                     <button><a href="delete.php?id=<?php echo $value['id']; ?>">delete</a></button>
+                </p>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</body>
 
+</html>
 
-                </div>
-            <?php endforeach; ?>
-
-        </div>
-</main>
-<?php //include_once ('footer.php');?>
+<?php //include_once ('footer.php');
+?>
--- inlog.php
+++ inlog.php
@@ -1,4 +1,21 @@
-<?php include_once ('includes/connect.php');
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/menu.css">
+    <link rel="stylesheet" href="./css/inlog.css">
+    <title>sushi</title>
+</head>
+
+<body>
+
+<div>
+    <?php include_once ('includes/connect.php');
 
 $message ="";
 //var_dump($_POST);
@@ -13,16 +30,26 @@ if (isset($_POST["email"]) && isset($_POST['password'])) {
         }
     }
 }
-      
-    
-
-
-
-
 ?>
+</div>
+
+<div class="inlogform">
 <form action="" method="post">
     <input type="email" name="email" id="">
     <input type="password" name="password" id="">
     <input type="submit" name="submit" value="login">
 </form>
 <?php //include_once ('footer.php')?>
+</div>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
